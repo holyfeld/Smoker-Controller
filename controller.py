6ef50e538e2402f5fe0de110e8f9d195d3f81ef2
Don Gray <don@donaldegray.com>
2018-03-01 10:46:44 -0500

    added the display datat to controller.py to make the test in controller_test.py happy.

--- controller.py
+++ controller.py
@@ -1,7 +1,21 @@
+import sys
+
 class Controller():
     def exit(self):
         return(0)
 
-
     def format_data(self):
         return("12:12:12, 1, 2, 3, 4")
+
+    def display_data(self):
+        str_value = self.format_data()
+        print (str_value)
+
+
+def main(argv):
+    controller = Controller()
+    controller.display_data()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
